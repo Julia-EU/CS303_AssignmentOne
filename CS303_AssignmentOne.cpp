ae28e045bc3cce6553b9af74bbc98d2ba757a4af
Julia-EU <113368370+Julia-EU@users.noreply.github.com>
2023-06-26 22:16:14 -0500

    Update CS303_AssignmentOne.cpp

--- CS303_AssignmentOne.cpp
+++ CS303_AssignmentOne.cpp
@@ -47,22 +47,93 @@ int main()
 		getline(inFile2, lines2);
 		istringstream line2(lines2);
 		while (line2 >> values2) {
-			count2 += 1;
+			count2 += 1; //increments by total number of values per line till end of file. Final value is number of values in array
 		}
 		istringstream line3(lines2);
 		for (int i = size; i < count2; i++) {
 			line3 >> values3;
 			array[i] = stoi(values3);
 		}
-		size += count2 - size;
+		size += count2 - size; //increments by count minus previous size to match starting index value for each line
 	}
 	inFile2.close();
 
-	
-
+	cout << "The text file contains the following values:\n";
 	printValues(array, count2);
-	//addValue(array, 500, count2);
-	//printValues(array, count2);
-	//resetValue(array, 3, count2);
-	//printValues(array, count2);
+	cout << endl;
+
+	char inUse = 'Y'; //Variable character condition for while loop
+	while (inUse == 'Y') { 
+
+		char userChoice = Display_Menu();
+
+		try {
+
+			if (userChoice == 'A') {
+				string value;
+				cout << "Please type an integer value to search for: ";
+				cin >> value;
+				int avalue = stoi(value); //user input is converted to integer. if not compatible, exception is caught
+				searchValue(array, avalue, count2);
+				cout << endl;
+				cout << endl;;
+			}
+
+			else if (userChoice == 'B') {
+				string index;
+				string value;
+				cout << "Please enter the index number of the value you wish to change: ";
+				cin >> index;
+				int bindex = stoi(index);
+				cout << "\nPlease enter the value you wish to change it to: ";
+				cin >> value;
+				int bvalue = stoi(value);
+				changeValue(array, bvalue, bindex, count2);
+				cout << endl;
+				cout << "New array values are:\n";
+				printValues(array, count2);
+				cout << endl;
+			}
+
+			else if (userChoice == 'C') {
+				string value;
+				cout << "Please type an integer value to add to this array: ";
+				cin >> value;
+				int cvalue = stoi(value);
+				addValue(array, cvalue, count2);
+				cout << endl;
+				cout << "New array values are:\n";
+				printValues(array, count2);
+				cout << endl;
+			}
+
+			else if (userChoice == 'D') {
+				string index;
+				cout << "Please enter the index number of the value you wish to change: ";
+				cin >> index;
+				int dindex = stoi(index);
+				resetValue(array, dindex, count2);
+				cout << endl;
+				cout << "New array values are:\n";
+				printValues(array, count2);
+				cout << endl;
+			}
+
+			else {
+				inUse = 'N'; 
+			}
+
+		}
+
+		catch (invalid_argument& e) {
+			cout << e.what() << endl;
+			cout << endl;
+		}
+
+		catch (...) {
+			cout << "Invalid user input. Please enter a valid integer when prompted." << endl;
+			cout << endl;
+		}
+	}
+	
 }
