0760057ab5d7615e5fb1cc765614d5c5222e599f
Julia-EU <113368370+Julia-EU@users.noreply.github.com>
2023-06-26 22:15:13 -0500

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -2,12 +2,14 @@
 #include <iostream>
 using namespace std;
 
-void printValues(int array[], int arraySize);
+char Display_Menu(); //displays options for user interaction
 
-void searchValue(int array[], int value, int arraySize);
+void printValues(int array[], int arraySize); //prints array values
 
-void changeValue(int array[], int value, int index, int arraySize);
+void searchValue(int array[], int value, int arraySize); //searches array for value
 
-void resetValue(int* &array, int index, int& arraySize);
+void changeValue(int array[], int value, int index, int arraySize); //changes value at index specified
 
-void addValue(int* &array, int value, int &arraySize);
+void resetValue(int* &array, int index, int& arraySize); //modifies array size to change delete value or edits existing value to zero
+
+void addValue(int* &array, int value, int &arraySize); //modifies array size to add value
